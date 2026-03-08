9f417dd0b8ec961e3050694322427e3b32ff5c10
williblue <90781813+williblue@users.noreply.github.com>
2026-03-09 05:53:49 +0700

    Update IsometricRoom.tsx

--- src/components/IsometricRoom.tsx
+++ src/components/IsometricRoom.tsx
@@ -291,6 +291,8 @@ export default function IsometricRoom({
         height: canvasHeight,
         overflow: "hidden",
         zIndex: 1,
+        userSelect: "none",
+        WebkitUserSelect: "none",
       }}
     >
       {/* Isometric room PNG */}
